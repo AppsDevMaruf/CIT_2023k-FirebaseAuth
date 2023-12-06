93143bdf492d406502808294f5666bf1fe00abf2
MarufiMac <marufalam120@gmail.com>
2023-12-06 23:57:44 +0600

    convertTimestampToTime

--- app/src/main/java/com/maruf/firebaseauth/adapter/ChatAdapter.kt
+++ app/src/main/java/com/maruf/firebaseauth/adapter/ChatAdapter.kt
@@ -7,22 +7,28 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.maruf.firebaseauth.R
 import com.maruf.firebaseauth.data.chat.Chat
+import com.maruf.firebaseauth.utils.convertTimestampToTime
 
 
-class ChatAdapter(val chatList: List<Chat>, var myID: String) : RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+class ChatAdapter(private val chatList: List<Chat>, var myID: String) :
+    RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
     private val RIGHT = 1
     private val LEFT = 2
-    inner  class ChatViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val messageTv = view.findViewById<TextView>(R.id.messagetxt)
+
+    inner class ChatViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val messageTv: TextView = view.findViewById(R.id.messagetxt)
+        val time: TextView = view.findViewById(R.id.txtTime)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
 
         return if (viewType == RIGHT) {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.right_chat_ui, parent, false)
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.right_chat_ui, parent, false)
             ChatViewHolder(view)
         } else {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.left_chat_ui, parent, false)
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.left_chat_ui, parent, false)
             ChatViewHolder(view)
         }
 
@@ -35,6 +41,8 @@ class ChatAdapter(val chatList: List<Chat>, var myID: String) : RecyclerView.Ada
     override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
         val chat = chatList[position]
         holder.messageTv.text = chat.message
+        holder.time.text = convertTimestampToTime(chat.timeStamp)
+
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -44,7 +52,6 @@ class ChatAdapter(val chatList: List<Chat>, var myID: String) : RecyclerView.Ada
             LEFT
         }
     }
-
 
 
 }
--- app/src/main/java/com/maruf/firebaseauth/utils/Utils.kt
+++ app/src/main/java/com/maruf/firebaseauth/utils/Utils.kt
@@ -1,7 +1,16 @@
 package com.maruf.firebaseauth.utils
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 object Utils {
     const val DummyImgLink = "https://avatars.githubusercontent.com/u/44240762?s=400&u=3d47d70fa5de9672024eebaea40d4fb80b6f157e&v=4"
 
 
+}
+fun convertTimestampToTime(timestamp: Long): String {
+    val dateFormat = SimpleDateFormat("h:mm a", Locale.getDefault())
+    val date = Date(timestamp)
+    return dateFormat.format(date)
 }
